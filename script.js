03a0f12429df1499db0d81742c6cff44e4ada5fa
NonoPir <sanoonas100@gmail.com>
2023-07-18 17:37:25 +0400

    Refined JS code

--- script.js
+++ script.js
@@ -29,22 +29,21 @@ const assignClass = function (buttons) {
 
 assignClass(arr);
 
-sub.addEventListener("click", function () {
-  hide.classList.add("hidden");
-  phone.classList.remove("hidden");
-  secondHeader.classList.remove("hidden");
-  secondP.classList.remove("hidden");
+const addHiddenClass = (variable) => variable.classList.add("hidden");
+
+const removeHiddenClass = (variable) => variable.classList.remove("hidden");
+
+const submitRating = function () {
+  addHiddenClass(hide);
+  removeHiddenClass(phone);
+  removeHiddenClass(secondHeader);
+  removeHiddenClass(secondP);
   rating.style.backgroundColor = "hsl(216, 16%, 18%)";
-  value.classList.remove("hidden");
-  if (button1.classList.contains("button-clicked")) {
-    value.textContent = "You selected 1 out of 5";
-  } else if (button2.classList.contains("button-clicked")) {
-    value.textContent = "You selected 2 out of 5";
-  } else if (button3.classList.contains("button-clicked")) {
-    value.textContent = "You selected 3 out of 5";
-  } else if (button4.classList.contains("button-clicked")) {
-    value.textContent = "You selected 4 out of 5";
-  } else if (button5.classList.contains("button-clicked")) {
-    value.textContent = "You selected 5 out of 5";
-  } else value.textContent = "You selected 0 out of 5";
-});
+  removeHiddenClass(value);
+
+  arr.forEach(function (button, i, arr) {
+    button.classList.contains("button-clicked")
+      ? (value.textContent = `You selected ${i + 1} out of ${arr.length}`)
+      : "";
+  });
+};
